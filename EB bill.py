91102711c5b9037c724408c70aadfb1c98301360
pavithra192224034 <123437608+pavithra192224034@users.noreply.github.com>
2023-07-21 22:40:01 +0530

    Add files via upload

---
+++ EB bill.py
@@ -0,0 +1,14 @@
+units = int(input("Enter the number of units consumed: "))
+
+if units < 100:
+    amount = 20
+elif units <= 200:
+    amount = 20 + (units - 100) * 3
+elif units <= 400:
+    amount = 20 + 300 + (units - 200) * 5
+elif units <= 500:
+    amount = 20 + 300 + 1000 + (units - 400) * 7
+else:
+    amount = 20 + 300 + 1000 + 700 + (units - 500) * 10
+
+print("Your electricity bill is: Rs.", amount)
