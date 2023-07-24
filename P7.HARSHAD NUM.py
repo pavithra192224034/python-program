c2798784b68b95bff7e07aea7a742f064478d9dc
pavithra192224034 <123437608+pavithra192224034@users.noreply.github.com>
2023-07-23 18:21:05 -0700

    Add files via upload

---
+++ P7.HARSHAD NUM.py
@@ -0,0 +1,18 @@
+n=int(input("enter the digits : "))
+num=int(input("enter the number : "))
+num1=str(num)
+if(len(num1)!=n):
+    print("invalid")
+else:
+    temp=num 
+    sum=0
+   
+while(temp>0):
+    digit=temp%10
+    sum=sum+digit
+    temp=temp//10
+if num%sum==0:
+    print("harshad")
+else:
+    print("not harshad")
+
